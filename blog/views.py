22d7f2073e421f2f546fb9e3a41cc5089e661437
v.tyzhinov <v.tyzhinov@gmail.com>
2021-09-05 23:11:33 +0300

    Change mysite/blog/views.py past: post_share function

--- blog/views.py
+++ blog/views.py
@@ -48,15 +48,21 @@ def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status='published')
     sent = False
 
+
+def post_share(request, post_id):
+    # Retrieve post by id
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+
     if request.method == 'POST':
         # Form was submitted
         form = EmailPostForm(request.POST)
-        if form.is_valid:
+        if form.is_valid():
             # Form fields passed validation
             cd = form.cleaned_data
             post_url = request.build_absolute_uri(
                 post.get_absolute_url())
-            subject = f"{cd['name']} recomends you read " \
+            subject = f"{cd['name']} recommends you read " \
                       f"{post.title}"
             message = f"Read {post.title} at {post_url}\n\n" \
                       f"{cd['name']}\'s comments: {cd['comments']}"
@@ -68,30 +74,3 @@ def post_share(request, post_id):
     return render(request, 'blog/post/share.html', {'post': post,
                                                     'form': form,
                                                     'sent': sent})
-
-
-# def post_share(request, post_id):
-#     # Retrieve post by id
-#     post = get_object_or_404(Post, id=post_id, status='published')
-#     sent = False
-#
-#     if request.method == 'POST':
-#         # Form was submitted
-#         form = EmailPostForm(request.POST)
-#         if form.is_valid():
-#             # Form fields passed validation
-#             cd = form.cleaned_data
-#             post_url = request.build_absolute_uri(
-#                 post.get_absolute_url())
-#             subject = f"{cd['name']} recommends you read " \
-#                       f"{post.title}"
-#             message = f"Read {post.title} at {post_url}\n\n" \
-#                       f"{cd['name']}\'s comments: {cd['comments']}"
-#             send_mail(subject, message, 'v.tyzhinov@gmail.com',
-#                       [cd['to']])
-#             sent = True
-#     else:
-#         form = EmailPostForm()
-#     return render(request, 'blog/post/share.html', {'post': post,
-#                                                     'form': form,
-#                                                     'sent': sent})
